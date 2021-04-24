4a50f8921545970be1a4d021dd1f62bf8943461b
mthomsen77 <michael2004thomsen@hotmail.com>
2021-04-24 15:27:15 +0300

    Add skillset styles

--- components/skillset.js
+++ components/skillset.js
@@ -5,30 +5,41 @@ export default function Skillset(props) {
     console.log("props", props)
     return (
         <section className="dark-bg">
-          <div className="content-wrapper">
-            <div className="">
-                <h3 className={styles.title}>{props.data.title}</h3>
-            </div> 
-            
-            
-            <div className={styles['skillset-wrapper']}>
-            {props.data.skills.map((value, index) => {
-                console.log(value, index)
-                return (
-                <div key={index}>
-                    <Image
-                        src={value.icon}
-                        alt="Picture of the author"
-                        width={500}
-                        height={500}
-                    />
-                   <h4 className={styles['box-title']}>{value.title}</h4> 
-                   <p className={styles['box-description']}>{value.description}</p> 
-                </div>
-                )
-            })}
-            </div>
-            
+            <div className="inner-wrapper">
+                <div className="content-wrapper">
+                    <div className={styles.header}>
+                        <h3 className={styles.title}>{props.data.title}</h3>
+                        <div className={styles.border}></div>
+                    </div> 
+                    
+                    
+                    <div className={styles['skillset-wrapper']}>
+                    {props.data.skills.map((value, index) => {
+                        console.log(value, index)
+                        return (
+                        <div key={index} className={styles.skillset}>
+                            <Image
+                                className={styles.icon}
+                                src={value.icon}
+                                alt="Picture of the author"
+                                width={40}
+                                height={40}
+                                layout="fixed"
+                            />
+                            <div className={styles.text}>
+                                <h5 className={styles.title}>{value.title}</h5> 
+                                <p className={styles.description}>{value.description}</p>
+                                <div className={styles.barWrapper}>
+                                    <div className={styles.bar} style={{ width: `${value.percentage}%` }}></div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        )
+                    })}
+                    </div>
+                    
+                    </div>
             </div>
         </section>
     )    
