0884b8c1581c3d064c1b034083e78db173346bf6
mthomsen77 <michael2004thomsen@hotmail.com>
2021-06-09 23:28:04 +0300

    update nav colors

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,7 @@ export default class MyDocument extends Document {
           <meta property="og:site_name" content="MT Web" key="ogsitename" />
           <link rel="icon" href="/favicon2.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Ropa+Sans&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Ropa+Sans&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
