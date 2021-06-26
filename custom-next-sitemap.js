78f59d5d4b339f40eaa97a15cde37c8a3f0ede73
mthomsen77 <michael2004thomsen@hotmail.com>
2021-06-26 10:26:54 +0200

    Exclude urls in sitemap

--- custom-next-sitemap.js
+++ custom-next-sitemap.js
@@ -1,5 +1,6 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://mtweb.tech/',
     generateRobotsTxt: true, // (optional)
+    exclude: ['/github/authorizing', '/login']
     // ...other options
 }
